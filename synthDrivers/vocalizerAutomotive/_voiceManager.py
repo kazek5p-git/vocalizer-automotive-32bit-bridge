a51c839b4daef2a04278a2ed995c96187ae02bfe
djgraco <93210891+djgraco@users.noreply.github.com>
2026-08-08 10:43:53 +0200

    Update _voiceManager.py

--- synthDrivers/vocalizerAutomotive/_voiceManager.py
+++ synthDrivers/vocalizerAutomotive/_voiceManager.py
@@ -2,6 +2,7 @@
 #A part of the vocalizer driver for NVDA (Non Visual Desktop Access)
 #Copyright (C) 2012 Rui Batista <ruiandrebatista@gmail.com>
 #Copyright (C) 2012 - 2023 Tiflotecnia, lda. <www.tiflotecnia.net>
+#Copyright (C) 2026 DJ Graco and Kazek5p.
 #This file is covered by the GNU General Public License.
 #See the file GPL.txt for more details.
 
